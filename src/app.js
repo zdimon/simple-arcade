68471e5123efe466b6cda3e60d50296d685cc7c1
Your Name <you@example.com>
2020-09-18 15:13:46 +0300

    first commit

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
+
+
 $( document ).ready(() => {
 
 
     gameApp = {
+        hitball: 0,
         balls: [
-            {x: 0, y: 0, move: 'right'},
-            {x: 50, y: 50, move: 'right'}
+            {x: 20, y: 20, move: 'right'},
+            {x: 70, y: 70, move: 'right'}
         ],
         canon_x: 100,
         key_right: 'up',
@@ -19,6 +22,9 @@ $( document ).ready(() => {
 
 
     function draw(){
+
+        $('#appGame').append(`<div style="top:10px; left: 180px">Hit the ball: ${gameApp.hitball}</div>`);
+
         for (let ball of gameApp.balls){
             $('#appGame').append(`<div style="top: ${ball.y}px; left: ${ball.x}px" class="ball"></div>`);
         }
@@ -34,13 +40,19 @@ $( document ).ready(() => {
 
     function calc(){
 
+        console.log(typeof(gameApp.balls));
         for (let ball of gameApp.balls){
 
          if(ball.x>170) ball.move = 'left';
          if(ball.x<0) ball.move = 'right';
+         if(ball.y>170) ball.move = 'up';
+         if(ball.y<0) ball.move = 'down';
+
 
           if(ball.move === 'right') ball.x = ball.x+4;
           if(ball.move === 'left') ball.x = ball.x-4;
+          if(ball.move === 'down') ball.y = ball.y+4;
+          if(ball.move === 'up') ball.y = ball.y-4;
 
         }
 
@@ -50,9 +62,38 @@ $( document ).ready(() => {
             bullet.y -= 10;
             if(bullet.y<0) gameApp.bullets.splice(gameApp.bullets.indexOf(bullet),1);
 
+          
             // collision 
+            for (let ball of gameApp.balls){
+                var el = document.querySelector('.ball');
+                var style = getComputedStyle(el).height;
+                const size_ball = Math.floor(style.slice(0,-2)/2);
 
-            
+                var el = document.querySelector('#canon');
+                var style = getComputedStyle(el).top;
+                console.log('canony', style)
+                const canon_y = Math.floor(style.slice(0,-2));
+
+                console.log(size_ball);
+                console.log(bullet.x, bullet.y, ball.x, ball.y)
+                if ((bullet.y <= ball.y + size_ball && bullet.y >= ball.y - size_ball) && 
+                     (bullet.x <= ball.x+ size_ball && bullet.x >= ball.x - size_ball))
+                {
+                    gameApp.hitball += 1;
+                    gameApp.balls.splice(gameApp.balls.indexOf(ball),1);
+                    var rand_x = Math.floor(Math.random()*Math.floor(170));
+                    var y = 20;
+                    gameApp.balls.push({x: rand_x, y: y, move: 'down'});
+                    console.log('push',gameApp.balls);
+                    $('#appGame').append(`<div style="top: ${y}px; left: ${rand_x}px" class="ball"></div>`);
+                }
+                if ((gameApp.canon_x <= ball.x + size_ball && gameApp.canon_x >= ball.x - size_ball) && 
+                (canon_y <= ball.y + size_ball && canon_y >= ball.y - size_ball))
+                {
+                    alert('Hit canon! GAME OVER');
+                }
+
+            }
 
            }
 
